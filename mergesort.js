d0b94f061b2e0e2f8a463a3588547bcad9cc7780
Nikki Consolacion <nikkiconsolacion@gmail.com>
2019-11-16 11:48:57 -0800

    progress on merge

--- mergesort.js
+++ mergesort.js
@@ -3,3 +3,23 @@ function split(wholeArray) {
   const secondHalf = wholeArray.slice(wholeArray.length/2)
   return [firstHalf, secondHalf];
 }
+
+function merge(arr1, arr2) {
+  //[1,4, 5, 8], [2,3, 6, 7]
+  let result = [];
+  for(let i = 0; i < arr1.length; ++i) {
+    for (let j = 0; j < arr2.length; ++j){
+      if (arr1[i] > arr2[j]){
+        result.push(arr2[j]);
+        arr2.splice(0, 1);
+      }
+      else if (arr1[i] < arr2[j]) {
+        result.push(arr1[i]);
+        arr1.splice(0, 1);
+        result.push(arr2[j]);
+      }
+    }
+  }
+
+  return result;
+}
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,5 +1,11 @@
 describe('Split Array function', function() {
   it('is able to split an array into two halves', function() {
-    expect( split([1,2,3,4]) ).toEqual( [[1,2], [3,4]] );
+    expect( split([1,2,3,4,5]) ).toEqual( [[1,2], [3,4,5]] );
+  });
+});
+describe('Merge function', function(){
+  it('is able to merge two sorted arrays into one sorted array', function(){
+    // test the merging algorithm
+    expect( merge([1,4, 5, 8], [2,3, 6, 7]) ).toEqual( [1,2,3,4,5,6,7,8] );
   });
 });
